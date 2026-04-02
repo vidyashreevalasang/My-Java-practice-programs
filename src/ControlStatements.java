bbe85ca1d1dec53e0b84c1eddc3bb1d6a606f6fc
Honagoud Bagali <honagoud.bagali@gmail.com>
2026-04-02 15:56:08 +0530

    Add ControlStatements.java and other Java practice programs

---
+++ src/ControlStatements.java
@@ -0,0 +1,48 @@
+public class ControlStatements {
+
+
+    public void loopwhile()
+    {
+        int i=0;
+        while( i<=5){
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+    public void loopdowhile(){
+        int i=0;
+        do{
+
+            System.out.println("do while output:"+i);
+            i=i+1;
+        }while(i<=5);
+    }
+    public void jumpstatementsBreak()
+    {
+        for(int i=0;i<=5;i++){
+            if(i==2)
+                break;
+            System.out.println("break statement output:"+i);
+
+        }
+    }
+    public void jumpstatementContinue()
+    {
+        for(int i=0;i<=5;i++)
+        {
+            if(i==2)
+                continue;
+            System.out.println("continue statement output"+ i);
+
+        }
+    }
+    public static void main(String[] args) {
+        ControlStatements demo=new ControlStatements();
+//        demo.loopwhile();
+//        demo.loopdowhile();
+        demo.jumpstatementsBreak();
+        demo.jumpstatementContinue();
+
+
+    }
+}
